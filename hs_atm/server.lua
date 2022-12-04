e9ce51d2c3a88555d6110cbcf57df8bbfa724322
Fugascz <fugasovicv2@gmail.com>
2022-12-04 12:31:17 +0100

    better optimalization

--- hs_atm/server.lua
+++ hs_atm/server.lua
@@ -1,23 +1,23 @@
-ESX = nil
-
-TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
-
-
 local activity = 0
 local activitySource = 0
 
-RegisterNetEvent("item:buy")
-AddEventHandler("item:buy", function()
-    local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
+RegisterNetEvent("item:buy", function()
+    local client = source
+    local xPlayer = ESX.GetPlayerFromId(client)
 
-    xPlayer.removeMoney(cfg.money["price"])
-    xPlayer.addInventoryItem(cfg.item["atm"], 1)
+	if xPlayer then
+		local playerPed = GetPlayerPed(client)
+		local playerCoords = GetEntityCoords(playerPed)
+		local dist = #(cfg.zones["shop"] - playerCoords)
 
+		if dist < 5 then
+			xPlayer.removeMoney(cfg.money["price"])
+			xPlayer.addInventoryItem(cfg.item["atm"], 1)
+		end
+	end
 end)
 
-RegisterNetEvent("item:check")
-AddEventHandler("item:check", function()
+RegisterNetEvent("item:check", function()
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
 
@@ -28,43 +28,45 @@ AddEventHandler("item:check", function()
         TriggerClientEvent('esx:showNotification', source, cfg.translations["needitem"])
     end
 end)
-RegisterNetEvent("done:hack")
-AddEventHandler("done:hack", function(hackmoney, optimalizace)
-    local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-    if optimalizace == true then    
-        xPlayer.addAccountMoney('bank', hackmoney)
 
-    else
-    	xPlayer.kick("Kaboom")
+RegisterNetEvent("done:hack", function()
+    local client = source
+    local xPlayer = ESX.GetPlayerFromId(client)
+
+    if xPlayer then
+		if client == activitySource then
+			local money = cfg.money["random"]
+
+			xPlayer.addAccountMoney('bank', money)
+			xPlayer.showNotification(""..cfg.translations["successhacking"].. " "..hackmoney.."$")
+		else
+			-- MAYBE KICK?
+		end
     end        
 end)
-ESX.RegisterServerCallback('sd_atm:police',function(source, cb)
+
+
+lib.callback.register('sd_atm:police',function(source, cb)
     local anycops = 0
-    local playerList = ESX.GetPlayers()
-    for i=1, #playerList, 1 do
-      local _source = playerList[i]
-      local xPlayer = ESX.GetPlayerFromId(_source)
-      local playerjob = xPlayer.job.name
-      if playerjob == 'police' then
-        anycops = anycops + 1
-      end
-    end
-    cb(anycops)
+
+	for _, player in pairs(ESX.GetExtendedPlayers()) do
+		if player.job.name == 'police' then
+			anycops += 1
+		end
+	end
+
+    return anycops
 end)
-ESX.RegisterServerCallback('sd_atm:isActive',function(source, cb)
-    cb(activity)
-end)
-RegisterServerEvent('sd_atm:registerActivity')
-AddEventHandler('sd_atm:registerActivity', function(value)
+
+RegisterNetEvent('sd_atm:registerActivity', function(value)
 	activity = value
+
 	if value == 1 then
 		activitySource = source
-		local xPlayers = ESX.GetPlayers()
-		for i=1, #xPlayers, 1 do
-			local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-			if xPlayer.job.name == 'police' then
-				TriggerClientEvent('sd_atm:setcopnotification', xPlayers[i])
+
+		for _, player in pairs(ESX.GetExtendedPlayers()) do
+			if player.job.name == 'police' then
+				TriggerClientEvent('sd_atm:setcopnotification', player.source)
 			end
 		end
 	else
@@ -72,43 +74,36 @@ AddEventHandler('sd_atm:registerActivity', function(value)
 	end
 end)
 
-RegisterServerEvent('sd_atm:alertcops')
-AddEventHandler('sd_atm:alertcops', function(cx,cy,cz)
-	local source = source
-	local xPlayer = ESX.GetPlayerFromId(source)
-	local xPlayers = ESX.GetPlayers()
-	
-	for i=1, #xPlayers, 1 do
-		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-		if xPlayer.job.name == 'police' then
-			TriggerClientEvent('sd_atm:setcopblip', xPlayers[i], cx,cy,cz)
+RegisterNetEvent('sd_atm:alertcops', function(cx,cy,cz)
+	local client = source
+
+	for _, player in pairs(ESX.GetExtendedPlayers()) do
+		if player.job.name == 'police' then
+			TriggerClientEvent('sd_atm:setcopblip', player.source, cx, cy, cz)
 		end
 	end
 end)
 
-RegisterServerEvent('sd_atm:stopalertcops')
-AddEventHandler('sd_atm:stopalertcops', function()
-	local source = source
-	local xPlayer = ESX.GetPlayerFromId(source)
-	local xPlayers = ESX.GetPlayers()
-	for i=1, #xPlayers, 1 do
-		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-		if xPlayer.job.name == 'police' then
-			TriggerClientEvent('sd_atm:removecopblip', xPlayers[i])
+RegisterNetEvent('sd_atm:stopalertcops', function()
+	local client = source
+
+	for _, player in pairs(ESX.GetExtendedPlayers()) do
+		if player.job.name == 'police' then
+			TriggerClientEvent('sd_atm:removecopblip', player.source)
 		end
 	end
 end)
 
-AddEventHandler('playerDropped', function ()
-	local _source = source
-	if _source == activitySource then
-		local xPlayers = ESX.GetPlayers()
-		for i=1, #xPlayers, 1 do
-			local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-			if xPlayer.job.name == 'police' then
-				TriggerClientEvent('sd_atm:removecopblip', xPlayers[i])
+AddEventHandler('playerDropped', function()
+	local client = source
+
+	if client == activitySource then
+		for _, player in pairs(ESX.GetExtendedPlayers()) do
+			if player.job.name == 'police' then
+				TriggerClientEvent('sd_atm:removecopblip', player.source)
 			end
 		end
+
 		activity = 0
 		activitySource = 0
 	end
